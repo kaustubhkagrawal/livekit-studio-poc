5872d8b6adbfa64495b676db3f207b95d2ad2333
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2023-12-25 19:47:19 +0530

    trying the functions

--- apps/studio/src/config/env.config.ts
+++ apps/studio/src/config/env.config.ts
@@ -1,7 +1,12 @@
 export const envConfig = {
   conference: {
     livekit: {
-      wsUrl: 'wss://testico-qpok6ilf.livekit.cloud',
+      wsUrl:
+        import.meta.env.VITE_APP_LIVEKIT_WS_URL ||
+        'wss://testico-qpok6ilf.livekit.cloud',
+
+      apiKey: import.meta.env.VITE_APP_LIVEKIT_API_KEY || 'APIhSjLb7LG5CkY',
+      apiSecret: import.meta.env.VITE_APP_LIVEKIT_API_SECRET || '',
     },
   },
 };
