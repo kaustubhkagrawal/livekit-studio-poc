546e3b75f014470001894da7b0b6ac0b91008953
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2023-12-27 09:57:25 +0530

    generate identicon

--- libs/modules/studio/src/components/Studio/StudioAside.tsx
+++ libs/modules/studio/src/components/Studio/StudioAside.tsx
@@ -1,16 +1,33 @@
+import { RootState } from '@kaustubhkagrawal/shared';
 import { forwardRef } from 'react';
+import { useSelector } from 'react-redux';
 
 interface StudioAsideProps {}
 
 const StudioAside = forwardRef<HTMLDivElement, StudioAsideProps>(
   (props, ref) => {
+    const participants = useSelector(
+      (state: RootState) => state.participants.participants
+    );
+
     return (
       <aside
         ref={ref}
         className={`hidden md:flex flex-col w-full md:w-80 gap-2`}
       >
         <nav className="hstack justify-end w-full px-4 pt-4 gap-2">hi</nav>
-        <div className="hidden md:flex flex-1 mx-2 mb-4 rounded-lg bg-blue-400"></div>
+        <div className="hidden md:flex flex-1 mx-2 mb-4 px-4 py-4 rounded-lg gap-2 bg-blue-400">
+          {participants.map((participant) => (
+            <div className="vstack items-center max-w-20">
+              <img
+                src={`https://gravatar.com/avatar/${participant.sid.toLowerCase()}?d=identicon`}
+                alt={participant.identity}
+                className="rounded-full bg-blue-500 w-10 h-10"
+              ></img>
+              <div>{participant.identity}</div>
+            </div>
+          ))}
+        </div>
       </aside>
     );
   }
